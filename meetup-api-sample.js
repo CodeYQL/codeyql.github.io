7db188d607cbcd89a4cec8302303af4fc4bd10cf
Chris Rabl <rabl.christopher@gmail.com>
2018-01-16 23:40:55 -0700

    first commit

---
+++ meetup-api-sample.js
@@ -0,0 +1,48 @@
+// request 
+// curl https://api.meetup.com/CodeYQL/events\?\&sign\=true\&photo-ho<200c><200b>st\=public\&page\=20\&st<200c><200b>atus\=past,upcoming
+
+[
+  {
+    created: 1512076682000,
+    duration: 7200000,
+    id: "245539184",
+    name: "January Project Night",
+    status: "upcoming",
+    time: 1516410000000,
+    local_date: "2018-01-19",
+    local_time: "18:00",
+    updated: 1514414435000,
+    utc_offset: -25200000,
+    waitlist_count: 0,
+    yes_rsvp_count: 17,
+    venue: {
+      id: 23697650,
+      name: "tecconnect: An Alberta centre for new commerce",
+      lat: 49.735450744628906,
+      lon: -112.7999496459961,
+      repinned: false,
+      address_1: "3582 30 St. N.",
+      city: "Lethbridge",
+      country: "ca",
+      localized_country_name: "Canada",
+      zip: "",
+      state: "AB"
+    },
+    group: {
+      created: 1426040733000,
+      name: "CodeYQL",
+      id: 18492685,
+      join_mode: "open",
+      lat: 49.70000076293945,
+      lon: -112.81999969482422,
+      urlname: "CodeYQL",
+      who: "Nerds",
+      localized_location: "Lethbridge, AB",
+      region: "en_US"
+    },
+    link: "https://www.meetup.com/CodeYQL/events/245539184/",
+    description:
+      "<p>\u2022 What we'll do<br/>Commit to a small project, task or craft that you've been wanting to do. Our hack nights don't have to include code - you can do whatever you want.</p> <p>This is a social and focused event, with the purpose of helping people set aside some time to actually work on projects that they've been looking for time to do. If you need help or guidance, this is a great time to ask questions.</p> <p>If you don't have a project, feel free to come anyway, and perhaps do some pair programming or offer insight!</p> <p>Food and snack will be provided.</p> <p>\u2022 What to bring<br/>Bring whatever you need for your project! If your 3D printer is portable, by all means!</p> <p>Shout out to NerdLabYYC (<a href=\"https://www.meetup.com/NerdLab-YYC\" class=\"linkified\">https://www.meetup.com/NerdLab-YYC</a>) for giving us the idea.</p> ",
+    visibility: "public"
+  }
+];
